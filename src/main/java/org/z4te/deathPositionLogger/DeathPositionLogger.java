0af801fc7dd7a9a698c1f1af3926cf5b4697e925
Z4TE <kayakun426@gmail.com>
2024-12-11 19:06:42 +0900

    ディメンションの表示を追加

--- src/main/java/org/z4te/deathPositionLogger/DeathPositionLogger.java
+++ src/main/java/org/z4te/deathPositionLogger/DeathPositionLogger.java
@@ -1,8 +1,9 @@
 package org.z4te.deathPositionLogger;
 
-import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -10,13 +11,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public final class DeathPositionLogger extends JavaPlugin implements Listener {
 
-    private final HashMap<UUID, Location> deathLocationStore = new HashMap<>();
+    private final HashMap<UUID, HashMap<Location, World.Environment>> outerMap = new HashMap<>();
 
     @Override
     public void onEnable() {
@@ -34,10 +35,14 @@ public final class DeathPositionLogger extends JavaPlugin implements Listener {
     public void onPlayerDeath(PlayerDeathEvent event) {
 
         Player player = event.getEntity();
+        UUID playerId = player.getUniqueId();
         Location deathLocation = player.getLocation();
-        String world = event.getEntity().getWorld().getName();
+        World.Environment dimension =event.getEntity().getWorld().getEnvironment();
 
-        deathLocationStore.put(player.getUniqueId(), deathLocation);
+        HashMap<Location, World.Environment> innerMap = new HashMap<>();
+        innerMap.put(deathLocation, dimension);
+
+        outerMap.put(playerId, innerMap);
     }
 
     @EventHandler
@@ -46,17 +51,21 @@ public final class DeathPositionLogger extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         UUID playerId = player.getUniqueId();
 
-        if (deathLocationStore.containsKey(playerId)) {
-            Location location = deathLocationStore.get(playerId);
+        for (Map.Entry<UUID, HashMap<Location, World.Environment>> outerEntry : outerMap.entrySet()) {
+            HashMap<Location, World.Environment> innerMapValue = outerEntry.getValue();
 
-            int x = location.getBlockX();
-            int y = location.getBlockY();
-            int z = location.getBlockZ();
+            for (Map.Entry<Location, World.Environment> innerEntry : innerMapValue.entrySet()) {
+                Location location = innerEntry.getKey();
+                World.Environment environment = innerEntry.getValue();
 
-            String recoveryMessage = String.format("You died at " + ChatColor.GREEN + "[%d, %d, %d]",x ,y, z);
+                int x = location.getBlockX();
+                int y = location.getBlockY();
+                int z = location.getBlockZ();
 
-            player.sendMessage("");
+                String message = String.format("You died at " + ChatColor.GREEN + "[%d, %d, %d]" + ChatColor.RESET + " in %s", x, y, z, environment);
+                player.sendMessage(message);
+            }
         }
+        outerMap.remove(playerId);
     }
-
 }
